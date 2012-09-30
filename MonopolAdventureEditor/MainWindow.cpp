6ef264ab6830e6cb26147df64c85ea004f869d1b
Vinorcola <vinorcola@gmail.com>
2012-09-30 19:51:14 +0200

    Implémentation de la fenêtre principale (menus, actions, etc.)

--- MonopolAdventureEditor/MainWindow.cpp
+++ MonopolAdventureEditor/MainWindow.cpp
@@ -5,12 +5,77 @@
 
 
 MainWindow::MainWindow() :
-    QMainWindow()
+    QMainWindow(),
+    m_plateau(new Plateau),
+    m_vueCentrale(new QGraphicsView),
+    m_barreOutils(addToolBar(tr("Outils d'édition"))),
+    m_actionQuitter(new QAction(tr("Quitter"), this)),
+    m_actionAssistantCreation(new QAction(tr("Créer un nouveau plateau"), this)),
+    m_actionRegroupement(new QAction(tr("Regroupements"), this))
 {
     /* Configuration de la fenêtre.
      */
     setWindowIcon(QIcon("data/MonopolAdventureEditorIcon.png"));
     setWindowTitle("MonopolAdventureEditor");
     setWindowState(Qt::WindowMaximized);
+    
+    
+    
+    /* Configuration du widget central.
+     */
+    setCentralWidget(m_vueCentrale);
+    m_vueCentrale->setScene(m_plateau);
+    
+    
+    
+    /* Configuration des actions.
+     */
+    m_actionQuitter->setShortcut(Qt::CTRL + Qt::Key_Q);
+    connect(m_actionQuitter, SIGNAL(triggered()), this, SLOT(quitter()));
+    connect(m_actionAssistantCreation, SIGNAL(triggered()), this, SLOT(startAssistant()));
+    
+    
+    
+    /* Configuration des menus.
+     */
+    QMenu* menuJeu(menuBar()->addMenu(tr("MonopolAdventureEditor")));
+    menuJeu->addAction(m_actionAssistantCreation);
+    menuJeu->addSeparator();
+    menuJeu->addAction(m_actionQuitter);
+    
+    
+    
+    /* Configuration des barres d'outils.
+     */
+    m_barreOutils->addAction(m_actionRegroupement);
+    m_barreOutils->hide();
+    
+    
+    
+    /* Lancement de l'assistant de création de plateau.
+     */
+    QTimer::singleShot(500, this, SLOT(startAssistant()));
+}
+
+
+
+
+
+void MainWindow::quitter()
+{
+    qApp->quit();
+}
+
+
+
+
+
+void MainWindow::startAssistant()
+{
+    AssistantCreationPlateau* assistant(new AssistantCreationPlateau(m_plateau));
+    if (assistant->exec())
+    {
+        m_barreOutils->show();
+    }
 }
 
--- MonopolAdventureEditor/MainWindow.hpp
+++ MonopolAdventureEditor/MainWindow.hpp
@@ -1,8 +1,16 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
+#include <QAction>
 #include <QApplication>
+#include <QGraphicsView>
 #include <QMainWindow>
+#include <QMenuBar>
+#include <QTimer>
+#include <QToolBar>
+
+#include "assistants/creationPlateau/AssistantCreationPlateau.hpp"
+#include "donnees/Plateau.hpp"
 
 
 
@@ -17,8 +25,36 @@ class MainWindow : public QMainWindow
         Q_OBJECT
         
         
+    private:
+        Plateau* m_plateau;///< Plateau en cours d'édition.
+        QGraphicsView* m_vueCentrale;///< Widget central de la fenêtre.
+        QToolBar* m_barreOutils;///< Barre d'outils d'édition.
+        QAction* m_actionQuitter;///< Action permettant de quitter l'éditeur.
+        QAction* m_actionAssistantCreation;///< Action permettant de lancer l'assistant de création de plateau.
+        QAction* m_actionRegroupement;///< Action permettant d'éditer la liste des regroupements du plateau.
+        
+        
+        
     public:
+        /**
+         * Construit le fenêtre principale du programme.
+         */
         MainWindow();
+        
+        
+        
+    public slots:
+        /**
+         * Ferme l'application.
+         */
+        void quitter();
+        
+        
+        
+       /**
+        * Lance l'assistant de création de plateau.
+        */
+        void startAssistant();
 };
 
 #endif // MAINWINDOW_HPP
