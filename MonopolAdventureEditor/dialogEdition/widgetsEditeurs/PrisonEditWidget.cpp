cba52f241e1a1b1fc0d0bdd42879b82643918f34
Vinorcola <vinorcola@gmail.com>
2012-11-13 23:16:06 +0100

    Création de la classe PrisonEditWidget.

---
+++ MonopolAdventureEditor/dialogEdition/widgetsEditeurs/PrisonEditWidget.cpp
@@ -0,0 +1,36 @@
+#include "PrisonEditWidget.hpp"
+
+
+
+
+
+PrisonEditWidget::PrisonEditWidget(Prison* const prison) :
+    QWidget(),
+    m_prison(prison),
+    m_champCaution(new QSpinBox)
+{
+    /* Configuration des champs.
+     */
+    m_champCaution->setSingleStep(5);
+    m_champCaution->setMaximum(MONTANT_MAX_EDITEUR);
+    m_champCaution->setValue(m_prison->getCaution());
+    
+    
+    
+    /* Mise en forme du widget.
+     */
+    QFormLayout* layout = new QFormLayout;
+    layout->addRow(tr("Montant de la caution que doit payer le joueur pour sortir de prison"), m_champCaution);
+    
+    setLayout(layout);
+}
+
+
+
+
+
+void PrisonEditWidget::sauvegarde()
+{
+    m_prison->editCaution(m_champCaution.value());
+}
+
---
+++ MonopolAdventureEditor/dialogEdition/widgetsEditeurs/PrisonEditWidget.hpp
@@ -0,0 +1,39 @@
+#ifndef PRISONEDITWIDGET_HPP
+#define PRISONEDITWIDGET_HPP
+
+#include <QFormLayout>
+#include <QSpinBox>
+
+#include "donnees/emplacements/Prison.hpp"
+
+
+
+
+
+class PrisonEditWidget : public QWidget
+{
+        Q_OBJECT
+        
+        
+    private:
+        Prison* m_prison;///< Prison en cours d'édition.
+        QSpinBox m_champCaution;///< Champ d'édition de la caution.
+        
+        
+        
+    public:
+        /**
+         * Construit un nouveau widget d'édition de prison.
+         * @param prison Prison à éditer.
+         */
+        PrisonEditWidget(Prison* const prison);
+        
+        
+        
+        /**
+         * Edite les informations de la prison en fonction de la configuration des champs.
+         */
+        void sauvegarde();
+};
+
+#endif // PRISONEDITWIDGET_HPP
