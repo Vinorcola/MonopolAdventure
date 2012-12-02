593c434d59501be243daef59ec47b75eb34d0910
Vinorcola <vinorcola@gmail.com>
2012-12-02 23:59:01 +0100

    Suppression des quantités de maisons/hôtels dans la règle puisqu'elles sont déjà dans le terrain.

--- MonopolAdventureEditor/donnees/Regle.cpp
+++ MonopolAdventureEditor/donnees/Regle.cpp
@@ -5,45 +5,7 @@
 
 
 Regle::Regle() :
-    m_nombreMaisonsPourHotel(4),
-    m_nombreMaxHotel(1)
 {
     
-}
-
-
-
-
-
-quint8 Regle::getNombreMaisonsPourHotel() const
-{
-    return m_nombreMaisonsPourHotel;
-}
-
-
-
-
-
-void Regle::editNombreMaisonsPourHotel(const quint8 nombre)
-{
-    m_nombreMaisonsPourHotel = nombre;
-}
-
-
-
-
-
-quint8 Regle::getNombreMaxHotel() const
-{
-    return m_nombreMaxHotel;
-}
-
-
-
-
-
-void Regle::editNombreMaxHotel(const quint8 nombre)
-{
-    m_nombreMaxHotel = nombre;
 }
 
--- MonopolAdventureEditor/donnees/Regle.hpp
+++ MonopolAdventureEditor/donnees/Regle.hpp
@@ -1,8 +1,6 @@
 #ifndef REGLE_HPP
 #define REGLE_HPP
 
-#include <QtGlobal>
-
 
 
 
@@ -16,8 +14,6 @@
 class Regle
 {
     private:
-        quint8 m_nombreMaisonsPourHotel;
-        quint8 m_nombreMaxHotel;
         
         
         
@@ -26,38 +22,6 @@ class Regle
          * Construit une règle par défaut.
          */
         Regle();
-        
-        
-        
-        /**
-         * Renseigne le nombre de maisons nécessaires pour construire un hôtel.
-         * @return Nombre de maisons nécessaires pour construire un hôtel.
-         */
-        quint8 getNombreMaisonsPourHotel() const;
-        
-        
-        
-        /**
-         * Change le nombre de maisons nécessaire pour construire un hôtel.
-         * @param nombre Nouveau nombre de maisons.
-         */
-        void editNombreMaisonsPourHotel(const quint8 nombre);
-        
-        
-        
-        /**
-         * Renseigne le nombre maximum d'hôtels constructibles sur un terrain.
-         * @return Nombre maximum d'hôtels constructibles sur un terrain.
-         */
-        quint8 getNombreMaxHotel() const;
-        
-        
-        
-        /**
-         * Change le nombre maximum d'hôtels constructibles sur un terrain.
-         * @param nombre Nouveau nombre d'hôtel.
-         */
-        void editNombreMaxHotel(const quint8 nombre);
 };
 
 #endif // REGLE_HPP
