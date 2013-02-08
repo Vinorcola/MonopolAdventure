52d4541ba0942632c8290dd55447dac2eec005ce
Vinorcola <vinorcola@gmail.com>
2013-02-09 00:48:18 +0100

    [MA] Implémentation des fonctionnalités de base de la MainWindow.

--- MonopolAdventure/MainWindow.cpp
+++ MonopolAdventure/MainWindow.cpp
@@ -5,11 +5,47 @@
 
 
 MainWindow::MainWindow() :
-    QMainWindow()
+    QMainWindow(),
+    m_plateau(0),
+    m_vueCentrale(new QGraphicsView),
+    m_actionQuitter(new QAction(tr("Quitter"), this)),
+    m_actionLancerNouvellePartie(new QAction(tr("Nouvelle partie"), this))
 {
     /* Configuration de la fenêtre.
      */
     setWindowIcon(QIcon("data/MonopolAdventureIcon.png"));
-    setWindowTitle("MonopolAdventure");
+    setWindowTitle("MonopolAdventureEditor");
+    
+    
+    
+    /* Configuration du widget central.
+     */
+    setCentralWidget(m_vueCentrale);
+    
+    
+    
+    /* Configuration des actions.
+     */
+    m_actionQuitter->setShortcut(QKeySequence::Quit);
+    connect(m_actionQuitter, SIGNAL(triggered()), this, SLOT(quitter()));
+    m_actionLancerNouvellePartie->setShortcut(Qt::CTRL + Qt::Key_N);
+    
+    
+    
+    /* Configuration des menus.
+     */
+    QMenu* menuJeu(menuBar()->addMenu(tr("MonopolAdventure")));
+    menuJeu->addAction(m_actionLancerNouvellePartie);
+    menuJeu->addSeparator();
+    menuJeu->addAction(m_actionQuitter);
+}
+
+
+
+
+
+void MainWindow::quitter()
+{
+    qApp->quit();
 }
 
--- MonopolAdventure/MainWindow.hpp
+++ MonopolAdventure/MainWindow.hpp
@@ -1,8 +1,13 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
+#include <QAction>
 #include <QApplication>
+#include <QGraphicsView>
 #include <QMainWindow>
+#include <QMenuBar>
+
+#include "donnees/Plateau.hpp"
 
 
 
@@ -17,8 +22,29 @@ class MainWindow : public QMainWindow
         Q_OBJECT
         
         
+    private:
+        Plateau* m_plateau;///< Plateau sur lequel se déroule la partie.
+        QGraphicsView* m_vueCentrale;///< Vue sur le plateau.
+        QAction* m_actionQuitter;///< Action permettant de quitter MonopolAdventureEditor.
+        QAction* m_actionLancerNouvellePartie;///< Action permettant de lancer une nouvelle partie.
+        
+        
+        
     public:
+        /**
+         * Construit la fenêtre principale du programme.
+         * 
+         * Construit les différentes actions, menus, barres d'outils, etc. Agence le contenu avec des layouts et connecte les diverses signaux et slots.
+         */
         MainWindow();
+        
+        
+        
+    public slots:
+        /**
+         * Quitte MonopolAdventure.
+         */
+        void quitter();
 };
 
 #endif // MAINWINDOW_HPP
